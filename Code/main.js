62b48e3354f0b40e03825848cf25068deb759b7e
Vishwas <gopinav5@gmail.com>
2018-07-22 19:17:55 +0530

    Variable declarations

--- Code/main.js
+++ Code/main.js
@@ -1,2 +1,8 @@
-var message = 'Hello World!';
+"use strict";
+exports.__esModule = true;
+var message = 'Welcome back!';
 console.log(message);
+var x = 10;
+var y = 20;
+var sum;
+var title = 'Codevolution';
--- Code/main.ts
+++ Code/main.ts
@@ -1,3 +1,9 @@
 export {};
-let message = 'Hello World!';
+let message = 'Welcome back!';
 console.log(message);
+
+let x = 10;
+const y = 20;
+
+let sum;
+const title = 'Codevolution';
