fa1aa0329cc2a12141934425f2cc24b9c82ce7e0
Vishwas <gopinav5@gmail.com>
2018-07-24 17:25:10 +0530

    Basic Types

--- Code/main.js
+++ Code/main.js
@@ -2,7 +2,14 @@
 exports.__esModule = true;
 var message = 'Welcome back!';
 console.log(message);
+// Variable Declaration
 var x = 10;
 var y = 20;
 var sum;
 var title = 'Codevolution';
+// Variable Types
+var isBeginner = true;
+var total = 0;
+var name = 'Vishwas';
+var sentence = "My name is " + name + "\nI am a beginner in TypeScript";
+console.log(sentence);
--- Code/main.ts
+++ Code/main.ts
@@ -2,8 +2,19 @@ export {};
 let message = 'Welcome back!';
 console.log(message);
 
+// Variable Declaration
 let x = 10;
 const y = 20;
 
 let sum;
 const title = 'Codevolution';
+
+// Variable Types
+let isBeginner: boolean = true;
+let total: number = 0;
+let name: string = 'Vishwas';
+
+let sentence: string = `My name is ${name}
+I am a beginner in TypeScript`;
+
+console.log(sentence);
