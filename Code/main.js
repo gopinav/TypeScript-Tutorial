85bed2531785e8906a5e7cae621e4718915e55fd
Vishwas <gopinav5@gmail.com>
2018-07-28 16:58:48 +0530

    Classes

--- Code/main.js
+++ Code/main.js
@@ -1,4 +1,14 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = Object.setPrototypeOf ||
+        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 exports.__esModule = true;
 var message = 'Welcome back!';
 console.log(message);
@@ -65,3 +75,30 @@ var p = {
     firstName: 'Bruce'
 };
 fullName(p);
+// Classes
+var Employee = /** @class */ (function () {
+    function Employee(name) {
+        this.employeeName = name;
+    }
+    Employee.prototype.greet = function () {
+        console.log('Good morning ' + this.employeeName);
+    };
+    return Employee;
+}());
+var emp1 = new Employee('Vishwas');
+console.log(emp1.employeeName);
+emp1.greet();
+var Manager = /** @class */ (function (_super) {
+    __extends(Manager, _super);
+    function Manager(managerName) {
+        return _super.call(this, managerName) || this;
+    }
+    Manager.prototype.delegateWork = function () {
+        console.log('Manager delgating tasks' + this.employeeName);
+    };
+    return Manager;
+}(Employee));
+var m1 = new Manager('Bruce');
+m1.delegateWork();
+m1.greet();
+console.log(m1.employeeName);
--- Code/main.ts
+++ Code/main.ts
@@ -89,3 +89,35 @@ let p = {
   firstName: 'Bruce'
 };
 fullName(p);
+
+// Classes
+
+class Employee {
+  employeeName: string;
+
+  constructor(name: string) {
+    this.employeeName = name;
+  }
+
+  greet() {
+    console.log('Good morning ' + this.employeeName);
+  }
+}
+
+let emp1 = new Employee('Vishwas');
+console.log(emp1.employeeName);
+emp1.greet();
+
+class Manager extends Employee{
+  constructor(managerName: string) {
+    super(managerName);
+  }
+  delegateWork() {
+    console.log('Manager delgating tasks' + this.employeeName);
+  }
+}
+
+let m1 = new Manager('Bruce');
+m1.delegateWork();
+m1.greet();
+console.log(m1.employeeName);
