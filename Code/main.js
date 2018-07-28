cccb74bc9f78866bfdcc637254640d4c78cfc737
Vishwas <gopinav5@gmail.com>
2018-07-28 07:53:45 +0530

    Optional and Default Parameters

--- Code/main.js
+++ Code/main.js
@@ -48,3 +48,13 @@ multiType = true;
 var anyType;
 anyType = 20;
 anyType = true;
+// Functions
+function add(num1, num2) {
+    if (num2 === void 0) { num2 = 10; }
+    if (num2)
+        return num1 + num2;
+    else
+        return num1;
+}
+add(5, 10);
+add(5);
--- Code/main.ts
+++ Code/main.ts
@@ -64,9 +64,12 @@ anyType = true;
 
 // Functions
 
-function add(num1: number, num2?: number): number {
-  return num1 + num2;
+function add(num1: number, num2: number = 10): number {
+  if (num2)
+    return num1 + num2;
+  else
+    return num1;
 }
 
 add(5, 10);
-add(10);
+add(5);
